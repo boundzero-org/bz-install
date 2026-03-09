9cc343f7038cb3e7281d0f0310351c5b4807ef88
HustenCodes <hustenlafleur@outlook.com>
2026-03-08 17:55:22 -0700

    feat: add 'early' os install setup script

---
+++ os-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+### BoundZero OS Main install script
+### (Ran when creating the Installation Media in the Cubic Shell)
+
+set -exuo pipefail
+
+BZ_TOOLS_INSTALL_DIR="/opt/bz-tools/"
+
+git clone https://github.com/boundzero-org/bz-tools.git $BZ_TOOLS_INSTALL_DIR/bz-tools || true
+git clone https://github.com/boundzero-org/bz-install.git $BZ_TOOLS_INSTALL_DIR/bz-install || true
+
+cd /etc/skel
+mkdir Documents || true
+
+sudo ln -s $BZ_TOOLS_INSTALL_DIR/bz-install/os-post-install.sh Documents/
