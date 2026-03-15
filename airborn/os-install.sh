ff4646eb163a72deb09b8d9dbd5d2adc332a5e61
HustenCodes <hustenlafleur@outlook.com>
2026-03-15 14:45:19 -0700

    feat: os file mirror to install pipeline

--- airborn/os-install.sh
+++ airborn/os-install.sh
@@ -5,13 +5,41 @@
 
 set -exuo pipefail
 
-BZ_TOOLSET_INSTALL_DIR="/opt/boundzero"
+copy_files_preserve_dirs () {
+    local source_dir="$1"
+    local dest_dir="$2"
+
+    sudo mkdir -p "$dest_dir"
+
+    find "$source_dir" -type f | while read -r file; do
+        
+        relative_path="$(realpath -s --relative-to="$source_dir" "$file")"
+        dest_file="$dest_dir/$relative_path"
+        
+        dest_dir_path="$(dirname "$dest_file")"
+        sudo mkdir -p "$dest_dir_path"
+        
+        sudo cp "$file" "$dest_file"
+        echo "Copied: $relative_path"
+    done
+
+    echo "Copy complete!"
+}
+
+
 OS_RELEASE="airborn"
+OS_VERSION="0.3.2"
+OS_NAME="BoundZero OS \"${OS_RELEASE^}\" v$OS_VERSION"
 
-git clone https://github.com/boundzero-org/bz-tools.git $BZ_TOOLSET_INSTALL_DIR/bz-tools || true
-git clone https://github.com/boundzero-org/bz-install.git $BZ_TOOLSET_INSTALL_DIR/bz-install || true
+BZ_TOOLSET_INSTALL_DIR="/opt/boundzero"
 
-cd /etc/skel
-mkdir Documents || true
+git clone https://github.com/boundzero-org/bz-tools.git "$BZ_TOOLSET_INSTALL_DIR/bz-tools" || true
+git clone https://github.com/boundzero-org/bz-install.git "$BZ_TOOLSET_INSTALL_DIR/bz-install" || true
 
-sudo ln -s $BZ_TOOLSET_INSTALL_DIR/bz-install/$OS_RELEASE/os-post-install.sh Documents/
+
+
+copy_files_preserve_dirs "$BZ_TOOLSET_INSTALL_DIR/bz-install/$OS_RELEASE/os" /
+
+sudo ln -s "$BZ_TOOLSET_INSTALL_DIR/bz-install/$OS_RELEASE/os-post-install.sh" "/etc/skel/Documents/"
+
+sed "s/\[OS_NAME\]/$OS_NAME/g" /etc/skel/Documents
---
+++ airborn/os/etc/skel/Documents/README.txt
@@ -0,0 +1,20 @@
+# WELCOME TO BOUNDZERO
+### (Removing your Restrictions)
+
+Thank you for purchasing a PC from BoundZero. Your currently running [OS_NAME] on Linux Mint 22.3 Xfce (/w KDE Plazma). If you ever need help, remember to search with the terms "Linux Mint" and/or "KDE Plazma" to get accurate results.
+
+## Bypassing School Restrictions
+
+BoundZero OS comes with a few methods on bypassing school wide network restrictions (aka blocks). It comes with 2 Free VPNs (Proton & Windscribe) that can be used.
+
+In the Lake Stevens School District I recommend using Windscribe by:
+ 1. Create a Free Account at home, at windscribe.com 
+    (10 GB max by default, but you can get unlimited for $3/month if you select "Build A Plan" at 'https://windscribe.com/upgrade'. Then just chose 2 countries AND "Unlimited Data + R.O.B.E.R.T")
+ 2. Sign in on Windscribe App
+ 3. Make sue the Left Middle Line reads '(OFF) TCP 443 >' ass that works best
+ 4. Hit the upside-down power button to turn on the VPN, (NOTE: Remember to turn it off after you are done, as there is a 2GB Usage limit per month unless you upgrade)
+ 5. Periodically stopping and restarting the VPN when internet stops
+
+---
+
+Need help? Want to send feedback? Contact use at boundzerobis@outlook.com!
