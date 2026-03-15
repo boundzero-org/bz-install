c2371f8f39da0b81ae295d4b2674d5fcc8a1c74b
HustenCodes <hustenlafleur@outlook.com>
2026-03-15 14:09:06 -0700

    fix(airborn/os-install): incorrect bz-tools install dir

--- airborn/os-install.sh
+++ airborn/os-install.sh
@@ -5,13 +5,13 @@
 
 set -exuo pipefail
 
-BZ_TOOLS_INSTALL_DIR="/opt/bz-tools/"
+BZ_TOOLSET_INSTALL_DIR="/opt/boundzero"
 OS_RELEASE="airborn"
 
-git clone https://github.com/boundzero-org/bz-tools.git $BZ_TOOLS_INSTALL_DIR/bz-tools || true
-git clone https://github.com/boundzero-org/bz-install.git $BZ_TOOLS_INSTALL_DIR/bz-install || true
+git clone https://github.com/boundzero-org/bz-tools.git $BZ_TOOLSET_INSTALL_DIR/bz-tools || true
+git clone https://github.com/boundzero-org/bz-install.git $BZ_TOOLSET_INSTALL_DIR/bz-install || true
 
 cd /etc/skel
 mkdir Documents || true
 
-sudo ln -s $BZ_TOOLS_INSTALL_DIR/bz-install/$OS_RELEASE/os-post-install.sh Documents/
+sudo ln -s $BZ_TOOLSET_INSTALL_DIR/bz-install/$OS_RELEASE/os-post-install.sh Documents/
